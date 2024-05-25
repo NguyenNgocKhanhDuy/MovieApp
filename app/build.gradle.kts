319883f2ac71aafe78b5f555f9cabb63bbbbea21
Ngan Ha <Vonamnganha0611@gmail.com>
2024-05-25 22:17:55 +0700

    tao navigation bar & dinh dang fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,4 +41,7 @@ dependencies {
     implementation ("com.google.code.gson:gson:2.11.0")
     implementation ("com.squareup.retrofit2:retrofit:2.11.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.11.0")
+
+    // google navigationbar
+    implementation ("com.google.android.material:material:1.4.0")
 }
