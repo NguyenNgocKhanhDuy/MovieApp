0c64c761da6fddc651a958e35037a24071263d84
NguyenNgocKhanhDuy <21130035@st.hcmuaf.edu.vn>
2024-06-04 15:52:54 +0700

    xử lý account

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,4 +86,8 @@ dependencies {
     implementation("com.google.android.gms:play-services-auth:21.0.0")
     implementation("com.google.firebase:firebase-database")
     implementation("androidx.recyclerview:recyclerview")
+
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+
 }
